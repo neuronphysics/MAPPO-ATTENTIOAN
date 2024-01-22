840a93f4a40e384d429bd4271b9d733cc95a963f
sstevec <su19chang@gmail.com>
2024-01-22 15:35:35 -0500

    debug, change config

--- onpolicy/scripts/train/train_meltingpot.py
+++ onpolicy/scripts/train/train_meltingpot.py
@@ -225,16 +225,16 @@ def load_test_args(args):
     args.substrate_name = "territory__rooms"
     args.num_agents = 9
     args.seed = 1
-    args.n_rollout_threads = 2
+    args.n_rollout_threads = 5
     args.use_wandb = False
     args.user_name = "sc_debug_test_name"
     # args.wandb_name = "zsheikhb"
     args.share_policy = False
     args.use_centralized_V = False
     args.use_attention = False
-    args.num_env_steps = 6000
+    args.num_env_steps = 500000
     args.log_interval = 1
-    args.episode_length = 1000
+    args.episode_length = 2000
     args.data_chunk_length = 10
     args.attention_module = 'SCOFF'
 
